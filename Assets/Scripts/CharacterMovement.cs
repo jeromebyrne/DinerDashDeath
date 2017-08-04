98f9c89c37abc2a86455d7243e6cd65caeb8f715
jeromeBEAST <jeromebyrne21@hotmail.com>
2017-08-04 12:02:59 -0700

    - updates

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float moveForce = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float h = Input.GetAxis("Horizontal") * moveForce;
+
+        var rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(new Vector2(h, 0));
+	}
+}
