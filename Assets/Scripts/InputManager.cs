c3c44c0eba3858aa0b62cb483f06771e6696ea69
jeromeBEAST <jeromebyrne21@hotmail.com>
2017-08-05 23:31:07 -0700

    - updates

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    const string kHorizontalAxis = "Horizontal";
+    const string kVerticalAxis = "Vertical";
+
+    const float kPlayerMoveX = 1.0f;
+
+    public CharacterMovement playerMovement = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float deltaMove = Input.GetAxis(kHorizontalAxis) * kPlayerMoveX;
+
+        if (deltaMove != 0.0f)
+        {
+            playerMovement.IncrementVelocityX(deltaMove);
+        }
+
+        if (Input.GetAxis(kVerticalAxis) > 0)
+        {
+            playerMovement.Jump();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+public class PlayerAnimationController : MonoBehaviour {
+
+    public Player player = null;
+    private CharacterMovement playerMovement = null;
+    public SkeletonAnimation skeletonAnimation = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerMovement = player.gameObject.GetComponent<CharacterMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TrackEntry trackEntry = skeletonAnimation.AnimationState.GetCurrent(0);
+
+        if (System.Math.Abs(playerMovement.GetCurrentVelocityX()) > 0.0f)
+        {
+            if (trackEntry.Animation.Name != "run")
+            {
+                PlayAnimation("run", true);
+            }
+        }
+        else
+        {
+            if (trackEntry.Animation.Name != "idle1")
+            {
+                PlayAnimation("idle1", true);
+            }
+        }
+    }
+
+    void PlayAnimation(string animationName, bool loop)
+    {
+        if (!skeletonAnimation)
+        {
+            return;
+        }
+
+        skeletonAnimation.AnimationState.SetAnimation(0, animationName, loop);
+    }
+}
